85f4cc0f5be80db4126b349e50a7da6c818ab003
Alley Manalio <amanalio@ufl.edu>
2016-06-30 13:55:48 -0400

    Fix indentation issues not fixed by editorconfig

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <?php get_header(); ?>
 
-  <!-- Begin Section Container -->
+    <!-- Begin Section Container -->
     <section class="row">
       <div class="twelve columns">
         <!-- Begin Loop -->
-        <?php
-          if ( have_posts() ) {
+        <?php if ( have_posts() ) {
             while ( have_posts() ) {
-              the_post(); ?>
+                the_post(); ?>
                 <?php
-                  if ( has_post_thumbnail() ) {
-                    the_post_thumbnail('thumbnail');
-                  }
+                    if ( has_post_thumbnail() ) {
+                        the_post_thumbnail('thumbnail');
+                    }
                 ?>
-              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
-              <?php the_excerpt(); ?>
-              <a href="<?php the_permalink(); ?>">Read More</a>
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>">Read More</a>
 
             <?php
             } //end while
-          } //end if
-        ?>
-        <!-- End Loop -->
-      </div>
+        } //end if
+    ?>
+    <!-- End Loop -->
+    </div>
     </section>
-  <!-- End Section Container -->
+    <!-- End Section Container -->
 
 <?php get_footer(); ?>
