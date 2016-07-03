9b9e217701fc4b400d26cd1a7c2db40047ee994a
Alley Manalio <amanalio@ufl.edu>
2016-07-03 07:50:17 -0400

    Fix additional indentation issues on remaining files

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
     <footer class="row">
-      <div class="eight columns">
-        <h3>Left Column</h3>
-      </div>
-      <div class="four columns">
-        <h3>Left Column</h3>
-      </div>
+        <div class="eight columns">
+            <h3>Left Column</h3>
+        </div>
+        <div class="four columns">
+            <h3>Left Column</h3>
+        </div>
     </footer>
-  <!-- End Footer -->
-  </div> <!-- End Container Div -->
-  <?php wp_footer(); ?>
+    <!-- End Footer -->
+    </div> <!-- End Container Div -->
+    <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?php bloginfo(); ?></title>
-  <?php wp_head('name'); ?>
+    <title><?php bloginfo(); ?></title>
+    <?php wp_head('name'); ?>
 
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
   <div class="container">
     <header class="row">
-      <div class="eight columns">
+        <div class="eight columns">
         <h1><a href="<?php $url = home_url('/'); echo $url; ?>">
-          <?php bloginfo('name'); ?></a></h1>
+            <?php bloginfo('name'); ?></a></h1>
         <h2><?php bloginfo('description'); ?></h2>
-      </div>
-      <div class"four columns">
+        </div>
+        <div class"four columns">
         <h3>Search Bar</h3>
-      </div>
+        </div>
     </header>
     <!-- MENU CODE BEGINS -->
     <div class="row">
-      <div class="twelve columns">
-        <?php
-          wp_nav_menu( array (
+        <div class="twelve columns">
+            <?php
+        wp_nav_menu( array (
             'sort_column' => 'menu_order',
             'container_class' => 'blank-menu-header'
-          ));
-        ?>
-      </div>
+        ));
+            ?>
+        </div>
     </div>
     <!-- MENU CODE ENDS -->
