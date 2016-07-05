48014c8196e6c5e8987ec309ce9d59b347280993
Alley Manalio <amanalio@ufl.edu>
2016-07-05 19:32:22 -0400

    Add custom post links

--- functions.php
+++ functions.php
@@ -39,8 +39,38 @@ add_action('widgets_init', 'blank_widgets_init');
 /*-------------- Enable Menu --------------- */
 add_theme_support('menus');
 
-/*--- Enable Post Thumbnails ---*/
+/*------ Enable Post Image Thumbnails ------*/
 add_theme_support( 'post-thumbnails' );
 
+/*-------------- Custom Post: Links--------------- */
+function custom_web_resources() {
+    $labels = array(
+        /*--- Begin Labels Options ---*/
+        'name'               => _x( 'Links', 'post type general name' ),
+        'singular_name'      => _x( 'Link', 'post type singular name' ),
+        'add_new'            => _x( 'Add New', weblink ),
+        'add_new_item'       => __( 'Add Link' ),
+        'edit_item'          => __( 'Edit Links' ),
+        'new_item'           => __( 'New Link' ),
+        'all_items'          => __( 'All Links' ),
+        'view_item'          => __( 'View Links' ),
+        'search_items'       => __( 'Search Links' ),
+        'not_found'          => __( 'No links found' ),
+        'not_found_in_trash' => __( 'No links found in the Trash' ),
+        'parent_item_colon'  => '',
+        'menu_name'          => 'Web Links'
+    );
+    $args = array(
+        /*--- Begin Arguments Options ---*/
+        'labels'        => $labels,
+        'description'   => 'Place to put useful links to other web resources',
+        'public'        => true,
+        'menu_position' => 5,
+        'supports'      => array('title', 'editor', 'thumbnail', 'excerpt', 'comments'),
+        'has_archive'   => true,
+    );
+    register_post_type('links', $args);
+}
+add_action ('init', 'custom_web_resources');
 
 ?>
