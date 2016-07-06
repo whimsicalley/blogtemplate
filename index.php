d4c5b3c71f45de1cab0bdc3662292bbf613c7326
Alley Manalio <amanalio@ufl.edu>
2016-07-06 00:24:05 -0400

    Change grid system

--- index.php
+++ index.php
@@ -2,32 +2,33 @@
 
     <!-- Begin Section Container -->
     <section class="row">
-        <div class="eight columns">
+        <!-- Begin Post Column -->
+        <div class="nine columns">
             <div class="row">
-            <!-- Begin Loop -->
-            <?php if ( have_posts() ) {
-                while ( have_posts() ) {
-                    the_post(); ?>
-                    <div class="row">
-                        <div class="three columns index-thumbnail">
-                            <?php if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('thumbnail');
-                            } ?>
-                        </div>
-                        <div class="nine columns">
-                            <h3 class="header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
-                                <?php the_excerpt(); ?>
-                            <a href="<?php the_permalink(); ?>">Read More</a>
-                        </div>
-                    </div>
-                    <?php
-                } //end while
-            } //end if
-            ?>
-            <!-- End Loop -->
+                <!-- Begin Post Loop -->
+                <?php if ( have_posts() ) {
+                    while ( have_posts() ) {
+                        the_post(); ?>
+                            <!--Start Individual Post Column-->
+                            <div class="four columns">
+                                <?php if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail('thumbnail');
+                                } ?>
+                                <h3 class="header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
+                                    <?php the_excerpt(); ?>
+                                <a href="<?php the_permalink(); ?>">Read More</a>
+                            </div>
+                            <!--End Post Column-->
+                            <?php
+                        } //end while
+                    } //end if
+                    ?>
+                <!-- End Loop -->
             </div>
         </div>
-        <div class="four columns">
+        <!-- End Post Column -->
+        <!-- Begin Sidebar Column-->
+        <div class="three columns">
             <?php dynamic_sidebar('first-index-widget'); ?>
         </div>
     </section>
