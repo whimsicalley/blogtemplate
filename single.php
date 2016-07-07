1889ded4a6a6ef112b886a9d23d39a089cdb486d
Alley Manalio <amanalio@ufl.edu>
2016-07-06 23:42:32 -0400

    Add open posts styling

--- single.php
+++ single.php
@@ -8,15 +8,15 @@
                 if ( have_posts() ) {
                     while ( have_posts() ) {
                         /* OUR DATA CONTEXT IS DEFINED */
-                        the_post();
+                        the_post(); ?>
 
-                            if ( has_post_thumbnail() ) { ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php if ( has_post_thumbnail() ) { ?>
                                 <div class="post-thumbnail">
                                     <?php the_post_thumbnail('large'); ?>
                                 </div>
                             <?php } ?>
 
-                        <h2><?php the_title(); ?></h2>
                         <?php the_content();
                     }
                 }
